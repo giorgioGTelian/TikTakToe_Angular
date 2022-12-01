f14c45fdbee0f6f5bf843d8683918c606c12303c
giorgioGTelian <giorgio.programmer@gmail.com>
2022-12-01 16:46:16 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TikTakToe_Angular
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-qhanuo)
---
+++ src/app/square/square.components.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import {Component, OnInit } from '@angular/core';
+
+@Component( {
+  selector: 'app-square',
+  template: 
+  <p>
+    square works!
+    </p>
+    ,
+    styles:[]
+})
+export class SquareComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {
+    
+  }
+}
